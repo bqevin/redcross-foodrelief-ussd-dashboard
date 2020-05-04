e275dca0e0d0874af9790443e84699eb5b26686b
Kevin Barasa <kevin.barasa001@gmail.com>
2020-05-04 22:46:16 +0300

    geography migration

---
+++ database/migrations/2020_05_04_194510_create_geography_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGeographyTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('geography', function (Blueprint $table) {
+            $table->id();
+            $table->string('location');
+            $table->string('ward');
+            $table->string('constituency');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('geography');
+    }
+}
