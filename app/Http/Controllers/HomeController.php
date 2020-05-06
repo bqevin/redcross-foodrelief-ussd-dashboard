ef3aa55bbf3d96964d7caab2134ccdfb607dbdcc
Kevin Barasa <kevin.barasa001@gmail.com>
2020-05-06 22:17:25 +0300

    refactor home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Complain;
 
 class HomeController extends Controller
 {
@@ -23,6 +23,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        return view('home', ['complains' => Complain::all()]);
     }
 }
