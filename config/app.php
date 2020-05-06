eda3118f8333f230ada7c054d66cf518bc94a98e
Kevin Barasa <kevin.barasa001@gmail.com>
2020-05-07 00:31:31 +0300

    defaul app name

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Mombasa Food Relief Complaints'),
 
     /*
     |--------------------------------------------------------------------------
