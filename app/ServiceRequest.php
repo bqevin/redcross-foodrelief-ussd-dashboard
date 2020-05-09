1c991a94de630e15817b732213548c6044864174
Kevin Barasa <kevin.barasa001@gmail.com>
2020-05-09 23:04:58 +0300

    service types mapping

--- app/ServiceRequest.php
+++ app/ServiceRequest.php
@@ -21,4 +21,33 @@ class ServiceRequest extends Model
     {
         return $this->belongsTo(GeoLocation::class);
     }
+
+    public function getServiceName(int $serviceNumber)
+    {
+
+        $serviceFullName = [
+            self::MISC => 'Miscellaneous',
+            self::OTHER_EMERGENCY => 'Other Emergency',
+            self::GARBAGE_COLLECTION => 'Garbage Collection',
+            self::FOOD => 'Relief Food Assistance',
+            self::HEALTH => 'Health Assistance',
+            self::COVID_INCIDENT => 'COVID-19 Case',
+            self::COVID_TESTING => 'COVID-19 Test'
+        ];
+
+        return $serviceFullName[ServiceRequest::servicesMap($serviceNumber)];
+    }
+
+    public static function servicesMap(int $serviceNumber)
+    {
+        return [
+            1 => self::COVID_TESTING,
+            2 => self::COVID_INCIDENT,
+            3 => self::HEALTH,
+            4 => self::FOOD,
+            5 => self::GARBAGE_COLLECTION,
+            6 => self::OTHER_EMERGENCY,
+            7 => self::MISC,
+        ][$serviceNumber];
+    }
 }
