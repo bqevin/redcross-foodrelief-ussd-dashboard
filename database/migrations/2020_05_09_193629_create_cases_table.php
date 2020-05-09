148b7f97ad6eebcfd8e4cd4e597246c9278c5064
Kevin Barasa <kevin.barasa001@gmail.com>
2020-05-09 19:40:25 +0300

    cases + requests migration

---
+++ database/migrations/2020_05_09_193629_create_cases_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCasesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cases', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('request_id')->unsigned()->nullable();
+            $table->text('details')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cases');
+    }
+}
