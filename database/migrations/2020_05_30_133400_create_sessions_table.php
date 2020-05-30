28cdf53ebed07cb241a5eeac484e98e9da34a23a
Kevin Barasa <kevin.barasa001@gmail.com>
2020-05-30 13:42:32 +0300

    session migration

---
+++ database/migrations/2020_05_30_133400_create_sessions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSessionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->id();
+            $table->string('at_id')->nullable();
+            $table->text('meta')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sessions');
+    }
+}
