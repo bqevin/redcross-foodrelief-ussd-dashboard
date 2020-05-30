d9a74912605204c77fbdc5e50573595796b967c3
Kevin Barasa <kevin.barasa001@gmail.com>
2020-05-30 12:24:33 +0300

    into sbadnmin2 template

---
+++ resources/views/sbadmin/all-feedback.blade.php
@@ -0,0 +1,65 @@
+@extends('sbadmin.layouts.app')
+<!-- Custom styles for this page -->
+<link href="{{ asset('sbadmin/vendor/datatables/dataTables.bootstrap4.min.css') }}" rel="stylesheet">
+@section('sbadmin.content')
+
+    <div class="container-fluid">
+
+        <!-- Page Heading -->
+        <h1 class="h3 mb-2 text-gray-800">Relief Food Distribution - Feedback</h1>
+        <p class="mb-4"> Mombasa residents feedback/complains or suggestions in relation to the ongoing relief food
+            distribution process</p>
+
+        <!-- DataTales Example -->
+        <div class="card shadow mb-4">
+            <div class="card-header py-3">
+                <h6 class="m-0 font-weight-bold text-primary">All Feedback </h6>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                        <thead>
+                        <tr>
+                            <th>TrackingID</th>
+                            <th>Feedback</th>
+                            <th>Location</th>
+                            <th>Village Elder</th>
+                            <th>Occurrence</th>
+                            <th>Reported</th>
+                        </tr>
+                        </thead>
+                        <tfoot>
+                        <tr>
+                            <th>TrackingID</th>
+                            <th>Feedback</th>
+                            <th>Location</th>
+                            <th>Village Elder</th>
+                            <th>Occurrence</th>
+                            <th>Reported</th>
+                        </tr>
+                        </tfoot>
+                        <tbody>
+                        @foreach ($feedback as $feed)
+                            <tr>
+                                <td>{{$feed->id}}</td>
+                                <td>{{$feed->description}}</td>
+                                <td>{{$feed->geoLocation ? $feed->geoLocation->location_description : 'N/A'}}</td>
+                                <td>{{$feed->official}}</td>
+                                <td>{{$feed->occurrence_date}}</td>
+                                <td>{{$feed->created_at->toDayDateTimeString()}}</td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <!-- Page level plugins -->
+    <script src="{{ asset('sbadmin/vendor/datatables/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('sbadmin/vendor/datatables/dataTables.bootstrap4.min.js') }}"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="{{ asset('sbadmin/js/demo/datatables-demo.js') }}"></script>
+@endsection
---
+++ resources/views/sbadmin/home.blade.php
@@ -0,0 +1,110 @@
+@extends('sbadmin.layouts.app')
+
+@section('sbadmin.content')
+    <div class="container-fluid">
+
+        <!-- Page Heading -->
+        <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+            <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
+                    class="fas fa-clock fa-sm text-white-50"></i> Last Updated: {{\Carbon\Carbon::now()->isoFormat('MMMM Do YYYY, h:mm:ss a')}}</a>
+        </div>
+
+        <!-- Content Row -->
+        <div class="row">
+
+            <!-- Earnings (Monthly) Card Example -->
+            <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-primary shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Total Issues <br>
+                                    (Processed)
+                                </div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{$issues}}</div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Services Request -->
+            <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-warning shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Service Requests <br>
+                                    (Old Town)
+                                </div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{$serviceRequests}}</div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fas fa-question fa-2x text-gray-300"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Relief Food Distribution Feedback -->
+            <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-info shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Feedback <br>
+                                    (Relief Food Distribution)
+                                </div>
+                                <div class="row no-gutters align-items-center">
+                                    <div class="col-auto">
+                                        <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">{{$feedback}}</div>
+                                    </div>
+                                    <div class="col">
+                                        <div class="progress progress-sm mr-2">
+                                            <div class="progress-bar bg-info" role="progressbar" style="width: 50%"
+                                                 aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Successful Requests -->
+            <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-success shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Application Interactions <br>
+                                    (Successful Traffic)
+                                </div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{$interaction}}</div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fas fa-phone fa-2x text-gray-300"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <!-- Page level plugins -->
+    <script src="{{ asset('sbadmin/vendor/chart.js/Chart.min.js') }}"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="{{ asset('sbadmin/js/demo/chart-area-demo.js') }}"></script>
+    <script src="{{ asset('sbadmin/js/demo/chart-pie-demo.js') }}"></script>
+@endsection
